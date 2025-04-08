7c44edca48e97f9487a7e82d369e376d6953c15e
Faxriddin <asqaraliyevfaxriddin2011@gmail.com>
2025-04-08 16:50:14 +0500

    qoshish

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function dev(a, b){
-    return a + b;
+function dev(a, b,c){
+    return a + b+c;
 }
 
-console.log(dev(10,2))
+console.log(dev(10,2,3))
+
+
+function dev2(){
+    return dev()
+    
+}
+
+dev2()
